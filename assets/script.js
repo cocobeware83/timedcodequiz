e7c0699684534b1231f8c9becafe2e4516c144ab
Coco <corycneel@gmail.com>
2020-12-31 22:39:00 -0600

    added quiz questions

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+// quiz questions //  
+var questions = [{
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<scripting>( )", "<javascript>( )", "<js>( )", "<script>( )"],
+        answer: "script( )"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function myFunction()( )", "function = myFunction()( )", "function:myFunction()( )"],
+        answer: "function myFunction()"
+    },
+    {
+        title: " How do you write an IF statement for executing code if 'i' is NOT equal to 5?",
+        choices: ["if (i != 5)( )", "if i <> 5( )", "if i =! 5 then( )", "if (i <> 5)"],
+        answer: "if (i != 5)( )"
+    },
+    {
+        title: "How can you add a comment in a JavaScript?",
+        choices: ["//This is a comment( )", "<!--This is a comment-->( )", "This is a comment( )"],
+        answer: "splice( )"
+    },
+    {
+        title: "What will the following code return: Boolean(10 > 9)",
+        choices: ["true( )", "false( )", " N/A( )", "NaN( )"],
+        answer: "true( )"
+    }
+]
+
